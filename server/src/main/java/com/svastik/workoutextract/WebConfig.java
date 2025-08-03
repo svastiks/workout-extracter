66b57b904f25e76be963a1e2a09f3fc1c3945dd5
svastik <svastiksharma13@gmail.com>
2025-08-02 21:24:29 -0400

    updated security config with domain

--- server/src/main/java/com/svastik/workoutextract/WebConfig.java
+++ server/src/main/java/com/svastik/workoutextract/WebConfig.java
@@ -14,10 +14,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "https://workoutextractor.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
